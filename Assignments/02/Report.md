f0598dc83a5c68a1caf8c1bfa51da519797c4ce3
Brandon Dale <bdale@blue.cs.sonoma.edu>
2023-10-16 16:18:02 -0700

    final version update

--- Assignments/02/Report.md
+++ Assignments/02/Report.md
@@ -2,12 +2,55 @@
 
 ## Improvement Iterations
 
-Here's a table showing the improvements I did to make the application go faster. The values below refer to testing on the test dataset. Speedup refers to user time
+Here's a table showing the improvements I did to make the application go faster. The values below refer to testing on the final dataset. Speedup refers to real time speedup.
 
-| Version | Time (Real) | Time (User) | Time (Sys) | Speedup | Changes |
+| Version | Time (Real) | Time (User) | Time (Sys) | Speedup (Real) | Changes |
 | :-----: | :-----: | :-----: | :-----: | :------: | ------- | 
-| 01 | 3.123s | 3.110s | 0.003s | &mdash; | Initial Version - No Changes |
-
+| 01 | 11m1.077s | 10m51.699s | 0m3.613s | &mdash; | Initial Version - No Changes |
+| 02 | 1m23.467s | 13m51.211s | 0m0.346s | 7.92% | Each thread calls getNext(number) and share a maxIter value and Records vector |
+| 03 | 1m8.646s  | 11m24.040s | 0m0.014s | 9.63% | Each thread cals getNext(number) and have their own maxIter and Records vector |
+| 04 | 1m9.739s  | &mdash;    | &mdash;  | 9.48% | This is from version 04b4 - See notes below for implementation notes |
+| 05 | 1m56.465s | 18m33.869  | 0m0.849s | 5.68% | Each thread has different batch sizes 1, 2, 4, 8, ... , 512 |
 
 ## Notes
-1. All times where obtained through testing on blue.
+1. All times where obtained through testing on blue at roughly the same time of day
+   on the same day.
+2. The User and Sys times have been lost for version 04, and that code has been lost. 
+   Since it is not the final submitted version, I have omitted those values. Though,
+   I can re-write that code to get those times if desired.
+
+
+## 04a Tests
+The following are testing values for version 04a.
+In this, batch sizes are regularly divided by 2 after every BatchIters
+where each batch means processing a vector of size BatchSize numbers.
+
+| Version | Time (Real) | Init. BatchSize | BatchIters |
+|:-----:  |:-----: | :-----: | :-----: |
+| 04a1 | 1m25.681s | 512 | 10 |
+| 04a1 | 1m25.633s | 1024 | 4 |
+
+
+## 04b Tests
+The following are testing values for version 04b.
+In this, the batch size is decremented after every batch by the
+decrement value.
+| Version | Time (Real) | Init. BatchSize | Decrement Value |
+|:-----:  |:-----: | :-----: | :-----: |
+| 04b1 | 1m10.596s | 512 | 1 |
+| 04b2 | 1m17.574s | 512 | 10 |
+| 04b3 | 1m11.549s | 256 | 1 |
+| 04b4 | 1m9.739s  | 128 | 1 |
+| 04b5 | 1m48.160s | 64  | 1 |
+
+## 04c Tests
+The following are testing values for version 04c.
+In this, batch sizes are static and are the same for each thread.
+
+| Version | Time (Real) | Init. BatchSize |
+|:-----:  |:-----: | :-----: |
+| 04c1 | 1m40.051s | 64 | 
+| 04c2 | 2m5.946s  | 128 |
+| 04c3 | 1m42.444s | 256 |
+| 04c4 | 1m43.197s | 512 |
+| 04c5 | 1m42.142s | 1024 |
--- Assignments/02/lychrel.cpp
+++ Assignments/02/lychrel.cpp
@@ -14,6 +14,9 @@
 #include <iomanip>
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <mutex>
+#include <barrier>
 
 #include "LychrelData.h"
 
@@ -38,87 +41,115 @@ int main() {
 
     std::cerr << "Processing " << data.size() << " values ...\n";
 
-    size_t maxIter = 0;  // Records the current maximum number of iterations
-    Records records; // list of values that took maxIter iterations
+    // std::atomic<size_t> maxIter = 0;  // Records the current maximum number of iterations
+    // Records records;     // list of values that took maxIter iterations
+    std::vector<size_t> maxIters(MaxThreads, 0);
+    std::vector<Records> t_records(MaxThreads);
 
-    // Iterate across all available data values, processing them using the 
-    //   reverse-digits and sum technique described in class.
-    for (auto i = 0; i < data.size(); ++i) {
-        Number number = data[i];
-        
-        size_t iter = 0;
-        Number n = number;
+    std::mutex mutex;    // protects the records vectors
+    std::barrier barrier(MaxThreads);
 
-        // The Lychrel loop - for any iteration, take the number, reverse
-        //   its digits, and sum those values together.  If that sum
-        //   is a palindrome, stop processing
-        while (!n.is_palindrome() && ++iter < MaxIterations) {
-            Number sum(n.size());   // Value used to store current sum of digits
-            Number r = n.reverse(); // reverse the digits of the value
+    // Start all threads
+    auto lastID = MaxThreads - 1;
+    for (auto id = 0; id < MaxThreads; ++id) {
 
-            // An iterator pointing to the first digit of the reversed
-            //   value.  This iterator will be incremented to basically
-            //   traverse the digits of the main number in reverse
-            auto rd = n.begin(); 
+        std::thread t{ [&]() {
+            Number number;
+
+            while (data.getNext(number)) {
+                size_t iter = 0;
+                Number n = number;
+
+                // The Lychrel loop - Check a single number n
+                while (!n.is_palindrome() && ++iter < MaxIterations) {
+                    Number sum(n.size());   // Value used to store current sum of digits
+                    Number r = n.reverse(); // reverse the digits of the value
+
+                    // An iterator pointing to the first digit of the reversed
+                    //   value.  This iterator will be incremented to basically
+                    //   traverse the digits of the main number in reverse
+                    auto rd = n.begin(); 
+                    
+                    bool carry = false;  // flag to indicate if we had a carry
+
+                    /*
+                    * Sum the digits using the "transform" algorithm.  This
+                    *   algorithm traverses a range of values (in our case,
+                    *   starting with the least-siginificant [i.e., right most]
+                    *   digit) of the original number, adding each digit to its
+                    *   matching digit (by position) in the reversed number.
+                    *
+                    * The result is stored in the sum variable, which is
+                    *   built up one digit at a time, respecting if a carry
+                    *   digit was necessary for any iteration. 
+                    */
+                    std::transform(n.rbegin(), n.rend(), sum.rbegin(), 
+                        [&](auto d) {
+                            auto v = d + *rd++ + carry;
             
-            bool carry = false;  // flag to indicate if we had a carry
+                            carry = v > 9;
+                            if (carry) { v -= 10; }
+            
+                            return v;
+                        }
+                    );
 
-            /*
-             * Sum the digits using the "transform" algorithm.  This
-             *   algorithm traverses a range of values (in our case,
-             *   starting with the least-siginificant [i.e., right most]
-             *   digit) of the original number, adding each digit to its
-             *   matching digit (by position) in the reversed number.
-             *
-             * The result is stored in the sum variable, which is
-             *   built up one digit at a time, respecting if a carry
-             *   digit was necessary for any iteration. 
-             */
-            std::transform(n.rbegin(), n.rend(), sum.rbegin(), 
-                [&](auto d) {
-                    auto v = d + *rd++ + carry;
-    
-                    carry = v > 9;
-                    if (carry) { v -= 10; }
-    
-                    return v;
+                    // If there's a final carry value, prepend that to the sum
+                    if (carry) { sum.push_front(1); }
+
+                    // Transfer the sum making it the next number to be processed
+                    //   (i.e., reversed, summed, and checked if it's a
+                    //   palindrome)
+                    n = sum;
                 }
-            );
 
-            // If there's a final carry value, prepend that to the sum
-            if (carry) { sum.push_front(1); }
+                /*
+                * Update our records.  First, determine if we have a new
+                *   maximum number of iterations that isn't the control limit
+                *   (MaxIterations) for a particular number.  If we're less
+                *   tha the current maximum (maxIter) or we've exceeded the number
+                *   of permissible iterations, ignore the current result and move
+                *   onto the next number.
+                */
+                if (iter < maxIters[id] || iter == MaxIterations) { continue; }
 
-            // Transfer the sum making it the next number to be processed
-            //   (i.e., reversed, summed, and checked if it's a
-            //   palindrome)
-            n = sum;
+                // Otherwise update our records, which possibly means discarding
+                //   our current maximum and rebuilding our records list.
+                
+                Record record{number, n};
+                if (iter > maxIters[id]) {    
+                    t_records[id].clear();
+                    maxIters[id] = iter;
+                }
+                t_records[id].push_back(record);
+            }
+
+            barrier.arrive_and_wait();
+        }};
+
+        (id < lastID) ? t.detach() : t.join();
+    }
+
+    // Process the list of each threads records
+    size_t maxIter = *max_element(maxIters.begin(), maxIters.end());
+    std::cout << "\nmaximum number of iterations = " << maxIter << "\n";
+    for (auto i = 0; i < MaxThreads; ++i) {
+        if (maxIters[i] == maxIter) {
+            for (auto& [number, palindrome] : t_records[i]) {
+                std::cout 
+                    << "\t" << number 
+                    << " : [" << palindrome.size() << "] "
+                    << palindrome << "\n";
+            }
         }
-
-        // Update our records.  First, determine if we have a new
-        //   maximum number of iterations that isn't the control limit
-        //   (MaxIterations) for a particular number.  If we're less
-        //   tha the current maximum (maxIter) or we've exceeded the number
-        //   of permissible iterations, ignore the current result and move
-        //   onto the next number.
-        if (iter < maxIter || iter == MaxIterations) { continue; }
-
-        // Otherwise update our records, which possibly means discarding
-        //   our current maximum and rebuilding our records list.
-        Record record{number, n};
-        if (iter > maxIter) {
-            records.clear();
-            maxIter = iter;
-        }
-
-        records.push_back(record);
     }
 
     // Output our final results
-    std::cout << "\nmaximum number of iterations = " << maxIter << "\n";
-    for (auto& [number, palindrome] : records) {
-        std::cout 
-            << "\t" << number 
-            << " : [" << palindrome.size() << "] "
-            << palindrome << "\n";
-    }
+    // std::cout << "\nmaximum number of iterations = " << maxIter << "\n";
+    // for (auto& [number, palindrome] : records) {
+    //     std::cout 
+    //         << "\t" << number 
+    //         << " : [" << palindrome.size() << "] "
+    //         << palindrome << "\n";
+    // }
 }
